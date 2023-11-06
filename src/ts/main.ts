44833b795559b34419f11477d420ffab7fc363b1
Kenan Schaefkofer <kbitikofer@gmail.com>
2023-11-05 21:18:31 -0800

    Attempt to get basic commands working

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,8 @@
 import { getCpmmProbability } from './lib/manifold/common/src/calculate-cpmm';
+import { getNewContract } from './lib/manifold/common/src/new-contract';
+import { ContractDictionary } from './lib/manifold/common/src/playground/contracts';
+import { Contract } from './lib/manifold/common/src/contract';
+import { User } from './lib/manifold/common/src/user';
 
 
 let submitFunction = function(event: JQuery.KeyUpEvent) {
@@ -30,7 +34,68 @@ let submitFunction = function(event: JQuery.KeyUpEvent) {
 // and display the output in div.output-container
 $('.command-input').on('keyup', submitFunction);
 
+let contract_dict: ContractDictionary = {};
+// Test user
+let user: User = {
+    id: '1',
+    createdTime: 0,
+    name: 'Alice',
+    username: 'alice',
+    avatarUrl: '',
+    balance: 1000,
+    totalDeposits: 0,
+    profitCached: {
+        daily: 0,
+        weekly: 0,
+        monthly: 0,
+        allTime: 0
+    },
+    creatorTraders: {
+        daily: 0,
+        weekly: 0,
+        monthly: 0,
+        allTime: 0
+    },
+    nextLoanCached: 0,
+    streakForgiveness: 0
+}
+
+
+
 function executeCommand(command: string): string {
-    // TODO: Implement command execution logic
-    return `Output for command "${command}"`;
+    // Split into tokens
+    const tokens: string[] = command.split(' ');
+    // The first token is the command
+    const commandName: string = tokens[0];
+    // The rest of the tokens are the arguments
+    const args: string[] = tokens.slice(1);
+
+    // CREATE command
+    if (commandName === 'CREATE') {
+        // Hard code the arguments for now
+        const contract = getNewContract(
+            '0x123', //id
+            'changeme', //slug
+            user,
+            'Will we succeed?', //question
+            'BINARY', //outcomeType
+            'This is a description', //description
+            0.5, //initialProb
+            10, //ante
+            undefined, //closeTime
+            'public', //visibility
+            false, //isTwitchContract
+            undefined, //min
+            undefined, //max
+            undefined, //isLogScale
+            undefined, //answers
+            undefined, //addAnswersMode
+            undefined, //shouldAnswersSumToOne
+            undefined, //loverUserId1
+            undefined //loverUserId2
+        );
+
+        return `Created contract ${contract.id}`;
+    }
+    return `Unknown command ${commandName}`;
 }
